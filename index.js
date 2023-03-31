9ecfd0e36256e781213ea02742dafe291f8298cf
Kennedy <kiplangatkennedy019@gmail.com>
2023-03-31 10:45:40 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function(){
+    let newText = document.getElementById("text")
+    return newText.textContent = "javascript is really cool!";
+
+})
